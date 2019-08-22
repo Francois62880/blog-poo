58452097e2f86711b127157c5b40ee3e1b709ebd
march <francois.cousin62880@gmail.com>
2019-08-22 23:35:48 +0200

    heritage

--- delete-comment.php
+++ delete-comment.php
@@ -9,6 +9,9 @@
 
 require_once('libraries/database.php');
 require_once('libraries/utils.php');
+require_once('libraries/models/Comment.php');
+
+$model = new Comment();
 
 /**
  * 1. Récupération du paramètre "id" en GET
@@ -21,20 +24,9 @@ $id = $_GET['id'];
 
 
 /**
- * 2. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- * PS : Vous remarquez que ce sont les mêmes lignes que pour l'index.php ?!
- */
-// $pdo = getPdo();
-
-
-/**
  * 3. Vérification de l'existence du commentaire
  */
-$commentaire = findComment($id);
+$commentaire = $model->find($id);
 if (!$commentaire) {
     die("Aucun commentaire n'a l'identifiant $id !");
 }
@@ -45,7 +37,7 @@ if (!$commentaire) {
  */
 $article_id = $commentaire['article_id'];
 
-deleteComment($id);
+$model->delete($id);
 
 /**
  * 5. Redirection vers l'article en question
--- index.php
+++ index.php
@@ -9,13 +9,22 @@
 
  require_once('libraries/database.php');
  require_once('libraries/utils.php');
+ require_once('libraries/models/Article.php');
+//  require_once('libraries/models/User.php');
 
+//  $userModel = new User();
+
+//  $users = $userModel-> findAll();
+//  var_dump($users);
+//  die();
+
+ $model = new Article();
 
 /**
  * 2. Récupération des articles
  */
 
-$articles = findAllArticles();
+$articles = $model ->findAll("created_at DESC");
 
 /**
  * 3. Affichage
